396a9be3a1d008a7222f017ca1de8cf1d77132d5
Abhinav <abhinavjha98ald@gmail.com>
2018-11-09 13:47:16 +0530

    Added the DOM EXAMPLE

---
+++ DOM_example/script.js
@@ -0,0 +1,10 @@
+var tag = document.getElementByid("highlight");
+console.log(tag);
+
+var tags = document.getElementsByClassName("bolded");
+console.log(tags[0]);
+
+var tags = document.getElementsByTagName("li");
+console.log(tags[0]);
+
+var tagss = document.querySelector('#highlight');
