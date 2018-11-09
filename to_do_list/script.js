6941215f350d26ee4fd54ed7e0948f4275190fd8
Abhinav <abhinavjha98ald@gmail.com>
2018-11-09 12:48:49 +0530

    Added the functions

--- to_do_list/script.js
+++ to_do_list/script.js
@@ -3,11 +3,31 @@ var todos = ["Buy new Tutle"];
 var input = prompt("What would you like to do?");
 while(input !=="quit"){
 	if(input === "list"){
-	console.log(todos);
+		listTodos();
 }else if(input === "new"){
-	var newTodo = prompt("Enter new todo");
-	todos.push(newTodo);
+	addTodo();
+}else if(input === "delete"){
+	deleteTodo();
 }
 input = prompt("What would you like to do?");
 }
 console.log("OK quitting the app")
+
+function listTodos(){
+	console.log("**********");
+	todos.forEach(function(todo,i){
+		console.log(i+" : "+todo);
+	});
+	console.log("**********");
+}
+
+function addTodo(){
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+	console.log("Added todos");
+}
+function deleteTodo(){
+var index = prompt("Enter index to be delete");
+	todos.splice(index,1);
+	console.log("Deleted todos");
+}
