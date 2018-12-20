f152502905ef5573f6078d7a7129647dd87c21e4
Abhinav <abhinavjha98ald@gmail.com>
2018-12-20 15:10:35 +0530

    Used the dom function

--- DOM_example/script.js
+++ DOM_example/script.js
@@ -1,11 +1,25 @@
-var tag = document.getElementByid("highlight");
-console.log(tag);
+// var tag = document.getElementByid("highlight");
+// console.log(tag);
 
-var tags = document.getElementsByClassName("bolded");
-console.log(tags[0]);
+// var tags = document.getElementsByClassName("bolded");
+// console.log(tags[0]);
 
-var tags = document.getElementsByTagName("li");
-console.log(tags[0]);
+// var tags = document.getElementsByTagName("li");
+// console.log(tags[0]);
 
-var tagss = document.querySelector('#highlight');
-console.log(tagss);
+// var tagss = document.querySelector('#highlight');
+// console.log(tagss);
+
+// var h1 = document.querySelector("h1");
+
+// h1.addEventListener("click",function(){
+// alert("hello")})
+
+var button = document.querySelector("button")
+var para = document.querySelector("h1")
+
+button.addEventListener("click",change);
+
+function change(){
+para.textContent = "Hello magic";
+}
